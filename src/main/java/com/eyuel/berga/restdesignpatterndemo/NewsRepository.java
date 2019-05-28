9522946fbbb729f902e399e5548ca467bd94b3e1
eyuel <eyuel.berga@yahoo.com>
2019-05-28 03:17:42 +0300

    small formatting change

--- src/main/java/com/eyuel/berga/restdesignpatterndemo/NewsRepository.java
+++ src/main/java/com/eyuel/berga/restdesignpatterndemo/NewsRepository.java
@@ -3,5 +3,4 @@ package com.eyuel.berga.restdesignpatterndemo;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface NewsRepository extends PagingAndSortingRepository<NewsDomain, Long> {
-
 }
